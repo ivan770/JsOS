ef11e1cfe76a46cfe76989bcc09761507884d4db
PROPHESSOR <xenteze@gmail.com>
2017-10-12 00:06:02 +0300

    changes

--- js/apps/vim/index.js
+++ js/apps/vim/index.js
@@ -7,36 +7,38 @@ const mauve = require('./mauve');
 const scheme = require('./lib/scheme');
 mauve.set(scheme);
 
-// Instance
-const vim = new Vim();
+function main(api) {
+    // Instance
+  const vim = new Vim();
 
-// Apply node commands (file write, etc.)
-require('./lib/commands')(vim);
+    // Apply node commands (file write, etc.)
+  require('./lib/commands')(vim);
 
-// Keystrokes
-Keys = require('terminal-keys');
-const keys = new Keys();
+    // Keystrokes
+  const Keys = require('terminal-keys');
+  const keys = new Keys();
 
-// Connect keys to vim instance
-keys.fn = function (key) {
+    // Connect keys to vim instance
+  keys.fn = function (key) {
     vim.exec(key);
-};
+  };
 
-// Clear the terminal screen
-tui.clear();
+    // Clear the terminal screen
+//   tui.clear();
 
-// Tie tui to vim.view
-vim.view.on('change', () => {
-    tui.write(vim.view.getText());
-});
+    // Tie tui to vim.view
+  vim.view.on('change', () => {
+    api.stdio.write(vim.view.getText());
+    // tui.write(vim.view.getText());
+  });
 
-// Open file if one has been indicated
-const files = []; // TODO: make this take cl args
-if (files.length) {
+    // Open file if one has been indicated
+  const files = []; // TODO: make this take cl args
+  if (files.length) {
     vim.exec(`:e ${files.shift()}\n`);
+  }
 }
-
 exports.commands = ['vim'];
 exports.call = (app, args, api) => {
-    api.stdio.writeLine('Works?');
+  main(api);
 };
