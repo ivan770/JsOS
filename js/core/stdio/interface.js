f3eaced18adc222b0a7a6e6e7f088902ecc0aba6
PROPHESSOR <xenteze@gmail.com>
2017-10-11 19:12:38 +0300

    Edited copyright

--- js/core/stdio/interface.js
+++ js/core/stdio/interface.js
@@ -1,4 +1,5 @@
 // Copyright 2014-present runtime.js project authors
+// Copyright 2017-present JsOS project authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
