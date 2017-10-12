cc622d3d3e3355001c88c527c0558ba8896e5864
UsernameAK <sitemakers.3dn.ru@gmail.com>
2017-10-12 23:47:52 +0300

    1024x768 24bpp white fill

--- js/core/graphics/init.js
+++ js/core/graphics/init.js
@@ -8,7 +8,13 @@ function init() {
     return false;
   }
 
-  $$.graphics.enableGraphics(1024, 768, 8);
+  $$.graphics.enableGraphics(1024, 768, 24);
+
+  debug($$.graphics.displayBuffer.length);
+
+  for (let i = 0; i < $$.graphics.displayBuffer.length; i++) {
+    $$.graphics.displayBuffer[i] = 0xff;
+  }
   
   return true;
 }
