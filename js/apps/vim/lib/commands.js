f211065d3cfec909a8effd1047d35545ec529c98
PROPHESSOR <xenteze@gmail.com>
2017-10-20 21:48:50 +0300

    Now VIM can write the files

--- js/apps/vim/lib/commands.js
+++ js/apps/vim/lib/commands.js
@@ -1,8 +1,5 @@
 'use strict';
 
-/* global $$ */
-
-// const fs = require('fs');
 const io = $$.stdio.defaultStdio;
 const fs = require('fs');
 
@@ -16,14 +13,6 @@ function main(Vim, exit) {
     },
   });
 
-  /* Vim.addCommand({
-    mode: 'command',
-    match: /^:(?:e|o) (.*)\n$/,
-    fn(keys, vim, match) {
-      vim.notify('Not implemented!');
-    },
-  }); */
-  // TODO: Write this
   Vim.addCommand({
     mode: 'command',
     match: /^:(?:e|o) (.*)\n$/,
@@ -32,29 +21,19 @@ function main(Vim, exit) {
       let text = '';
       // text = fs.readFileSync(filePath, 'binary');
       fs.readFile(filePath, (err, data) => {
-        if (err) return debug("Can't read the file!") ^ debug(err);
+        if (err) return debug("Can't read the file!") ^ debug(err); // TODO: Not debug message
 
         text = data;
 
         const doc = new vim.Doc();
         doc.path = filePath;
         doc.text(text);
-        // Tell vim all about it
         vim.add(doc);
       });
     },
   });
 
-
   Vim.addCommand({
-    mode: 'command',
-    match: /^:w\n$/,
-    fn(keys, vim, match) {
-      vim.notify('Not implemented!');
-    },
-  });
-  // TODO: Write this
-  /* Vim.addCommand({
     mode: 'command',
     match: /^:w\n$/,
     fn(keys, vim, match) {
@@ -63,20 +42,19 @@ function main(Vim, exit) {
         return vim.notify('E32: No file name');
       }
       const text = vim.curDoc.text();
-      try {
-        fs.writeFileSync(filePath, text, 'binary');
-      } catch (e) {
-        throw e;
-      }
-      const status = `"${filePath}" ${text.split('\n').length}L, ${text.length}C written`;
-      vim.view.set('status', status);
+      fs.writeFile(filePath, text, (err) => {
+        if (err) return debug('Can\'t write the file!') ^ debug(err); // TODO: Not debug message
+        const status = `"${filePath}" ${text.split('\n').length}L, ${text.length}C written`;
+        vim.view.set('status', status);
+      });
+      // fs.writeFileSync(filePath, text, 'binary');
     },
-  }); */
+  });
 
   Vim.addCommand({
     mode: 'command',
     match: 'zz', // ZZ
-    fn(keys, vim, match) {
+    fn(keys, vim /* , match */) {
       //   vim.exec(':w\n');
       vim.exec(':q\n');
     },
@@ -85,9 +63,9 @@ function main(Vim, exit) {
   Vim.addCommand({
     mode: 'command',
     match: '<C-c>',
-    fn(keys, vim, match) {
-      this.exec(':q\n');
-      this.notify('Type  :quit<Enter>  to exit Vim');
+    fn(keys, vim /* , match */) {
+      vim.exec(':q\n');
+      vim.notify('Type  :quit<Enter>  to exit Vim');
     },
   });
 }
