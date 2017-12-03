ae29a75b29ea669530959158ae93a841f22e5293
PROPHESSOR <xenteze@gmail.com>
2017-12-03 17:16:27 +0200

    Now example app have debug information

--- js/apps/example/index.js
+++ js/apps/example/index.js
@@ -8,9 +8,11 @@ function main(api, res) {
   io = api.stdio;
   io.setColor('green');
   io.writeLine('It works!!!');
+  const Arguments = JSON.stringify(arguments, null /* (x,y)=>typeof y === 'function' ? '[Function]' : y */, 4);
+  io.writeLine(`Arguments: ${Arguments}`);
   return res(0); // 1 = error
 }
 
-exports.call = (cmd, args, api, res) => main(api, res);
+exports.call = (cmd, args, api, res) => main(api, res, cmd, args);
 
 exports.commands = ['example'];
