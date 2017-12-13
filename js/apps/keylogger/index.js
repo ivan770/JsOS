260788bb94c47a8c32f1ce8052329fba3bab05f2
PROPHESSOR <xenteze@gmail.com>
2017-12-13 21:41:24 +0200

    Added mouse buttons to keylogger app

--- js/apps/keylogger/index.js
+++ js/apps/keylogger/index.js
@@ -4,11 +4,14 @@
 'use strict';
 let io,
   kb,
+  ms,
   resp;
 
 function main(api, res) {
   io = api.stdio;
   kb = api.keyboard;
+  ms = api.mouse;
+
   resp = res;
   io.setColor('green');
   io.writeLine('Keylogger started!');
@@ -16,7 +19,29 @@ function main(api, res) {
   io.writeLine('Press F12 for exit');
   io.setColor('pink');
   kb.onKeydown.add(keylog);
+  ms.onMousedown.add(mouselog);
   // return res(0); // 1 = error
+}
+
+function mouselog(key) {
+  let type;
+  switch (key) {
+    case 0:
+      type = 'Left button';
+      break;
+    case 1:
+      type = 'Middle button';
+      break;
+    case 2:
+      type = 'Right button';
+      break;
+    default:
+      type = 'Unknown mouse button';
+      break;
+  }
+
+  io.writeLine(type);
+  return false;
 }
 
 function keylog(key) {
@@ -29,6 +54,7 @@ function stop() {
   io.setColor('yellow');
   io.writeLine('Keylogger stoped');
   kb.onKeydown.remove(keylog);
+  ms.onMousedown.remove(mouselog);
   return resp(0);
 }
 
