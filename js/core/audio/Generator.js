f8cadb9b99daa3104936176cdf8d38c7ebba9c31
PROPHESSOR <xenteze@gmail.com>
2017-12-20 08:42:51 +0200

    Generator audio preset

---
+++ js/core/audio/Generator.js
@@ -0,0 +1,27 @@
+'use strict';
+
+class Generator {
+
+  static sine() {
+		// TODO:
+  }
+
+  static saw() {
+		// TODO:
+  }
+
+  static noise() {
+		// TODO:
+  }
+
+  static square() {
+		// TODO:
+  }
+
+  static triangular() {
+		// TODO:
+  }
+
+}
+
+module.exports = Generator;
--- js/core/audio/index.js
+++ js/core/audio/index.js
@@ -2,8 +2,21 @@
 
 class Audio {
   constructor() {
-    
+    // TODO:
+    // Super.... lol...
+  }
+
+  play() {
+    // TODO:
+  }
+
+  stop() {
+    // TODO:
+  }
+
+  static get currentDevice() {
+    return null;
   }
 }
 
-module.exports = new Audio;
+module.exports = Audio;
