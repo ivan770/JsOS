8a920921f8a3ecdbd1339a16c16199e091ac559d
PROPHESSOR <xenteze@gmail.com>
2018-02-10 13:56:41 +0200

    Printer is now a class

--- js/core/tty/printer.js
+++ js/core/tty/printer.js
@@ -1,4 +1,5 @@
 // Copyright 2015-present runtime.js project authors
+// Copyright 2018-present JsOS project authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,93 +18,103 @@
 const vga = require('./vga');
 const buffer = vga.allocBuffer();
 
-buffer.clear(vga.color.BLACK);
-
-let posCurrent = 0;
 const w = vga.WIDTH;
 const h = vga.HEIGHT;
+let posCurrent = 0;
 
-function refresh() {
-  vga.draw(buffer);
-}
+class Printer {
+  constructor() {
+    {
+      this.refresh = this.refresh.bind(this);
+      this.scrollUp = this.scrollUp.bind(this);
+      this.scrollDown = this.scrollDown.bind(this);
+      this.clear = this.clear.bind(this);
+      this.print = this.print.bind(this);
+      this.moveOffset = this.moveOffset.bind(this);
+      this.moveTo = this.moveTo.bind(this);
+    }
+    buffer.clear(vga.color.BLACK);
+    this.tmpcolor = [vga.color.WHITE, vga.color.BLACK];
+    this.color = vga.color;
+    this.refresh();
+  }
 
-function scrollUp() {
-  buffer.scrollUp(vga.color.BLACK);
-  posCurrent -= w;
-}
+  refresh() {
+    vga.draw(buffer);
+  }
 
-function scrollDown() {
-  buffer.scrollDown(vga.color.BLACK);
-  posCurrent -= w;
-}
+  scrollUp() {
+    buffer.scrollUp(vga.color.BLACK);
+    posCurrent -= w;
+  }
 
-function clear() {
-  buffer.clear(vga.color.BLACK);
-  posCurrent = 0;
-  refresh();
-}
+  scrollDown() {
+    buffer.scrollDown(vga.color.BLACK);
+    posCurrent -= w;
+  }
 
-refresh();
+  clear() {
+    buffer.clear(vga.color.BLACK);
+    posCurrent = 0;
+    this.refresh();
+  }
 
-exports.color = vga.color;
+  print(textOpt = '', repeat = 1, fg = vga.color.WHITE, bg = vga.color.BLACK) {
+    const text = String(textOpt);
 
-exports.print = (textOpt = '', repeat = 1, fg = vga.color.WHITE, bg = vga.color.BLACK) => {
-  // fix issue #53 where non-strings (ints, etc...) would not get printed
-  const text = String(textOpt);
-
-  for (let j = 0; j < repeat; ++j) {
-    for (const c of text) {
-      if (c === '\n') {
-        posCurrent -= (posCurrent % w) - w;
-        if (posCurrent >= w * h) {
-          scrollUp();
+    for (let j = 0; j < repeat; ++j) {
+      for (const c of text) {
+        if (c === '\n') {
+          posCurrent -= (posCurrent % w) - w;
+          if (posCurrent >= w * h) {
+            this.scrollUp();
+          }
+        } else {
+          if (posCurrent >= w * h) {
+            this.scrollUp();
+          }
+          buffer.setOffset(posCurrent++, c, fg, bg);
         }
-      } else {
-        if (posCurrent >= w * h) {
-          scrollUp();
-        }
-        buffer.setOffset(posCurrent++, c, fg, bg);
       }
     }
-  }
 
-  refresh();
-};
+    this.refresh();
+  };
 
-exports.moveOffset = (offsetOpt) => {
-  const offset = offsetOpt | 0;
-  let newPos = posCurrent + offset;
+  moveOffset (offsetOpt) {
+    const offset = offsetOpt | 0;
+    let newPos = posCurrent + offset;
 
-  if (newPos < 0) {
-    newPos = 0;
-  }
+    if (newPos < 0) {
+      newPos = 0;
+    }
 
-  if (newPos >= w * h) {
-    newPos = (w * h) - 1;
-  }
+    if (newPos >= w * h) {
+      newPos = (w * h) - 1;
+    }
 
-  posCurrent = newPos;
-};
+    posCurrent = newPos;
+  };
 
-exports.moveTo = (xOpt, yOpt) => {
-  let x = xOpt;
-  let y = yOpt;
+  moveTo (xOpt, yOpt) {
+    let x = xOpt;
+    let y = yOpt;
 
-  if (x < 0) {
-    x = 0;
-  }
-  if (x >= w) {
-    x = w - 1;
-  }
-  if (y < 0) {
-    y = 0;
-  }
-  if (y >= h) {
-    y = h - 1;
-  }
-  posCurrent = (y * w) + x;
-};
+    if (x < 0) {
+      x = 0;
+    }
+    if (x >= w) {
+      x = w - 1;
+    }
+    if (y < 0) {
+      y = 0;
+    }
+    if (y >= h) {
+      y = h - 1;
+    }
+    posCurrent = (y * w) + x;
+  };
+}
 
-exports.scrollUp = scrollUp;
-exports.scrollDown = scrollDown;
-exports.clear = clear;
+
+module.exports = new Printer();
